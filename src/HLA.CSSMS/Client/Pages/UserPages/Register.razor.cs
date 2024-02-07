32ed3238be12e8e04f802469a64f46d97aab2fcd
KenLWK <wklee1@hla.hongleong.com.my>
2024-02-07 12:07:00 +0800

    update

--- src/HLA.CSSMS/Client/Pages/UserPages/Register.razor.cs
+++ src/HLA.CSSMS/Client/Pages/UserPages/Register.razor.cs
@@ -8,10 +8,13 @@
         RegisterDto user = new RegisterDto();
         string message = string.Empty;
         string messageCssClass = string.Empty;
+        bool showLoading = false;
 
         async Task HandleRegistration()
         {
+            showLoading = true;
             var result = await AuthService.Register(user);
+            showLoading = false;
             message = result.Message;
             if (result.Success)
                 NavManager.NavigateTo("/login");
